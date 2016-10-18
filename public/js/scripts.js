9bc71f835155ee8bc4df1589ad5e0b4a22764d2a
teacher <a@a.com>
2016-10-18 21:29:53 +0400

    server

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('.exersise').on('change', 'input', function (e) {
         //e.target.id
-        alert(e.delegateTarget.id.substr(4) + ' ' + e.currentTarget.value);
+        //alert(e.delegateTarget.id.substr(4) + ' ' + e.currentTarget.value);
+        $('#'+e.delegateTarget.id).append($('<span style="color:red; font-size: 25px; font-weight: bold">+</span>'))
     });
 });
