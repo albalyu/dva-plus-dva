9d05191c23f581fc91a071bce3f27bcba9f8d1e8
teacher <a@a.com>
2016-10-18 21:26:00 +0400

    server

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.exersise').on('change', 'input', function (e) {
         //e.target.id
-        alert(e.delegateTarget.id.substr(4) + ' ' + e.currentTarget.val());
+        alert(e.delegateTarget.id.substr(4) + ' ' + e.currentTarget.val);
     });
 });
