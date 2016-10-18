956aefc15e07770d665deb8de20fad698757a368
teacher <a@a.com>
2016-10-18 21:35:51 +0400

    server

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $('.exersise').on('change', 'input', function (e) {
         var val = e.currentTarget.value;
-        $(this).html('<span>'+val+'</span>');
+        $(this).after('<span>'+val+'</span>');
+        $(this).remove();
         //e.target.id
         //alert(e.delegateTarget.id.substr(4) + ' ' + e.currentTarget.value);
         $('#'+e.delegateTarget.id).append($('<span style="color:red; font-size: 25px; font-weight: bold">+</span>'))
